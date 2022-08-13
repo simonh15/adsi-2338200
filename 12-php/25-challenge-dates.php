fc0007ac4345c2ca0c3ee448239ba17fb04180cd
simonh15 <simon.hoyos@misena.edu.co>
2022-08-13 11:45:45 -0500

    php

---
+++ 12-php/25-challenge-dates.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?="Hello World" ?> </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-info navbar-primary sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">PHP & Bootstrap V5</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Main Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">01 Hello World</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <main class="container">
+        <div class="row">
+            <div class="col-md-6 offset-3 text-center">
+            <div class="mb-3">
+                <label for="birthdate"> Birth Date </label>
+                <input type="date" id ="birthdate"  class="form-control" placeholder="mm/dd/yyyy">
+            </div>
+            <div class="mb-3">
+                <button type="submit " class="btn btn-outline-success">Calculate Age</button>
+            </div>
+           <?php 
+           
+            
+           ?>
+            </div>
+        </div>
+    
+    </main>
+</body>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</html>
